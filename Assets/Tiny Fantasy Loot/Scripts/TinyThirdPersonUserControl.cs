2180c035e70340782f9cb9956f7062d1ba2e3d45
hhassan1230 <hhassan@oswego.edu>
2018-11-11 01:17:53 -0500

    pushes script error and adds crystals to bullet example

--- Assets/Tiny Fantasy Loot/Scripts/TinyThirdPersonUserControl.cs
+++ Assets/Tiny Fantasy Loot/Scripts/TinyThirdPersonUserControl.cs
@@ -1,6 +1,6 @@
 using System;
 using UnityEngine;
-using UnityStandardAssets.CrossPlatformInput;
+//using UnityStandardAssets.CrossPlatformInput;
 
 namespace TinyDungeon
 {
@@ -48,21 +48,21 @@ namespace TinyDungeon
         private void FixedUpdate()
         {
             // read inputs
-            float h = CrossPlatformInputManager.GetAxis("Horizontal");
-            float v = CrossPlatformInputManager.GetAxis("Vertical");
+            float h; //= CrossPlatformInputManager.GetAxis("Horizontal");
+            float v;// = CrossPlatformInputManager.GetAxis("Vertical");
             //bool crouch = Input.GetKey(KeyCode.C);
 
             // calculate move direction to pass to character
             if (m_Cam != null)
             {
                 // calculate camera relative direction to move:
-                m_CamForward = Vector3.Scale(m_Cam.forward, new Vector3(1, 0, 1)).normalized;
-                m_Move = v*m_CamForward + h*m_Cam.right;
+               // m_CamForward = Vector3.Scale(m_Cam.forward, new Vector3(1, 0, 1)).normalized;
+                // m_Move = v*m_CamForward + h*m_Cam.right;
             }
             else
             {
                 // we use world-relative directions in the case of no main camera
-                m_Move = v*Vector3.forward + h*Vector3.right;
+                // m_Move = v*Vector3.forward + h*Vector3.right;
             }
 #if !MOBILE_INPUT
 			// walk speed multiplier
