7371c5a49592d14001f39bdd4fcc1af2d82fba18
hhassan1230 <hhassan@oswego.edu>
2018-11-17 01:42:24 -0500

    adds enemy test scene

---
+++ Assets/EnemyHealth.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour {
+	public int health;
+	private bool hasDied = false;
+
+	// Bond Script
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	void KillEnemies() {
+		// Disappears
+
+		// Timeout
+
+		// SetActive
+
+	}
+
+	public void HurtEnemy() {
+		 health -= 10;
+		 if (health <= 0) {
+		 	if (!hasDied) {
+			 	KillEnemies();
+				hasDied = true;
+		 	}
+		 }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
