3f544b782cb4a2fc0253d77746893a724c783788
hhassan1230 <hhassan@oswego.edu>
2018-11-28 22:36:45 -0500

    POC for ground level

---
+++ Assets/ComeInDelay.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ComeInDelay : MonoBehaviour {
+    public float delay = 2f;
+	// Use this for initialization
+	void Start () {
+        gameObject.SetActive(false);
+        Invoke("TurnOn", delay);
+
+	}
+
+    void TurnOn() {
+        gameObject.SetActive(true);
+    }
+}
---
+++ Assets/EnemyLookAt.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyLookAt : MonoBehaviour {
+    public Transform target;
+	// Use this for initialization
+	
+	// Update is called once per frame
+	void Update () {
+        transform.LookAt(target);
+	}
+}
